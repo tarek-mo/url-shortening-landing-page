759f632b63d428742a1ae99fa581c3afe63a9321
tarek-mo <tarekttik@gmail.com>
2022-07-16 12:37:27 +0100

    removed unused imports

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import BoostLinksToday from "../../components/BoostLinksToday";
 import Hero from "../../components/Hero";
-import ShortenLink from "../../components/ShortenLink";
 import Statistics from "../../components/Statistics";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      {/* <ShortenLink /> */}
       <Statistics />
       <BoostLinksToday />
     </>
